5e40e19990840130b5e80e544fe593f4d3aa083c
ER-RADA Oumaima <erradaoumaima@gmail.com>
2024-08-29 23:30:18 +0100

    Ajouter les méthodes toString aux classes Event, Participant et Registration

--- src/Event.java
+++ src/Event.java
@@ -1,4 +1,74 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
 
 public class Event {
+
+    // Insertion des variables
+    private int id;
+    private String title;
+    private String description;
+    private String location;
+    private LocalDate date;
+
+    // Constructeur
+    public Event(int id, String title, String description, String location, LocalDate date) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.location = location;
+        this.date = date;
+    }
+
+    // Getters et setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", location='" + location + '\'' +
+                ", date=" + date +
+                '}';
+    }
+
 }
--- src/Participant.java
+++ src/Participant.java
@@ -1,4 +1,50 @@
-package PACKAGE_NAME;
-
 public class Participant {
+
+    // Les attributs :
+    private int id;
+    private String name;
+    private String phoneNumber;
+
+    // Constructeur
+    public Participant(int id, String name, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    // Getters et Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Participant{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+
 }
